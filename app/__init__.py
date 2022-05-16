97a2ff2aa7f0d245b54e551fd83222fc6e50437f
Karenkoech <karenkoech3@gmail.com>
2022-05-16 15:04:11 -0400

    app initialization and functionality

--- app/__init__.py
+++ app/__init__.py
@@ -4,9 +4,43 @@ from os import path
 from flask_login import LoginManager
 
 
+db=SQLAlchemy()
+DB_NAME = 'db.sqlite'
+
+
 def create_app():
     app=Flask(__name__)
-    
-    
+    app.config['SECRET_KEY'] = '8ba2381251346f4daa053c7ef7d907fa'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+
+
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views,url_prefix='/')
+    app.register_blueprint(auth,url_prefix='/')
+
+    from .models import User,Post,Comment,Vote
+     
+    create_database(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view='auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
+
+
+
 
     return app
+ 
+def create_database(app):
+    if not path.exists("app/" + DB_NAME):
+        db.create_all(app=app)
+        print("Database created")
+    
